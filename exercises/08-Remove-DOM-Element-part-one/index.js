f2dfbef6ea37d965251005fb827d28688fba4f3e
gonzaloalonzo <gonzaloalonzo@hotmail.com>
2020-05-20 19:38:28 -0400

    ejercicio 7-10

--- exercises/08-Remove-DOM-Element-part-one/index.js
+++ exercises/08-Remove-DOM-Element-part-one/index.js
@@ -1,1 +1,7 @@
 //your code here
+let element = document.querySelector("#secondElement");
+element.parentNode.removeChild(element);
+/*var item = document.getElementById("secondElement"");
+item.parentNode.removeChild(item);*/
+
+// work both way!!
--- exercises/10-Render-on-Click/index.js
+++ exercises/10-Render-on-Click/index.js
@@ -1,6 +1,13 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
+	
 
+		
+		var para = document.createElement("div");
+		para.innerHTML ="<div>Hello World</div>";
+		document.querySelector("div").style.backgroundColor = "yellow";
+		document.body.append(para);
+	
 
 });
