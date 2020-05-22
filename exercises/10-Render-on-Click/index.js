8c5153eb18edeaa61395060787c2dfeb9af3a834
gonzaloalonzo <gonzaloalonzo@hotmail.com>
2020-05-21 20:13:01 -0400

    ejercicio 10 logrado

--- exercises/10-Render-on-Click/index.js
+++ exercises/10-Render-on-Click/index.js
@@ -1,13 +1,12 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
-	
-
-		
-		var para = document.createElement("div");
-		para.innerHTML ="<div>Hello World</div>";
+			
+		var para = document.createElement("DIV");
+		para.innerHTML ="Hello World";
+		document.body.appendChild(para);	
 		document.querySelector("div").style.backgroundColor = "yellow";
-		document.body.append(para);
+		
 	
-
+		
 });
